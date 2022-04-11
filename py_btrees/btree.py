748af217a962060d24deda88c8f7ada09d72456b
sandeepsangole <sandeepsangole@gmail.com>
2022-04-11 07:25:41 -0700

    first commit

---
+++ py_btrees/btree.py
@@ -0,0 +1,84 @@
+import bisect
+from typing import Any, List, Optional, Tuple, Union, Dict, Generic, TypeVar, cast, NewType
+from py_btrees.disk import DISK, Address
+from py_btrees.btree_node import BTreeNode, KT, VT, get_node
+
+"""
+----------------------- Starter code for your B-Tree -----------------------
+
+Helpful Tips (You will need these):
+1. Your tree should be composed of BTreeNode objects, where each node has:
+    - the disk block address of its parent node
+    - the disk block addresses of its children nodes (if non-leaf)
+    - the data items inside (if leaf)
+    - a flag indicating whether it is a leaf
+
+------------- THE ONLY DATA STORED IN THE `BTree` OBJECT SHOULD BE THE `M` & `L` VALUES AND THE ADDRESS OF THE ROOT NODE -------------
+-------------              THIS IS BECAUSE THE POINT IS TO STORE THE ENTIRE TREE ON DISK AT ALL TIMES                    -------------
+
+2. Create helper methods:
+    - get a node's parent with DISK.read(parent_address)
+    - get a node's children with DISK.read(child_address)
+    - write a node back to disk with DISK.write(self)
+    - check the health of your tree (makes debugging a piece of cake)
+        - go through the entire tree recursively and check that children point to their parents, etc.
+        - now call this method after every insertion in your testing and you will find out where things are going wrong
+3. Don't fall for these common bugs:
+    - Forgetting to update a node's parent address when its parent splits
+        - Remember that when a node splits, some of its children no longer have the same parent
+    - Forgetting that the leaf and the root are edge cases
+    - FORGETTING TO WRITE BACK TO THE DISK AFTER MODIFYING / CREATING A NODE
+    - Forgetting to test odd / even M values
+    - Forgetting to update the KEYS of a node who just gained a child
+    - Forgetting to redistribute keys or children of a node who just split
+    - Nesting nodes inside of each other instead of using disk addresses to reference them
+        - This may seem to work but will fail our grader's stress tests
+4. USE THE DEBUGGER
+5. USE ASSERT STATEMENTS AS MUCH AS POSSIBLE
+    - e.g. `assert node.parent != None or node == self.root` <- if this fails, something is very wrong
+
+--------------------------- BEST OF LUCK ---------------------------
+"""
+
+# Complete both the find and insert methods to earn full credit
+class BTree:
+    def __init__(self, M: int, L: int):
+        """
+        Initialize a new BTree.
+        You do not need to edit this method, nor should you.
+        """
+        self.root_addr: Address = DISK.new() # Remember, this is the ADDRESS of the root node
+        # DO NOT RENAME THE ROOT MEMBER -- LEAVE IT AS self.root_addr
+        DISK.write(self.root_addr, BTreeNode(self.root_addr, None, None, True))
+        self.M = M # M will fall in the range 2 to 99999
+        self.L = L # L will fall in the range 1 to 99999
+
+    def insert(self, key: KT, value: VT) -> None:
+        """
+        Insert the key-value pair into your tree.
+        It will probably be useful to have an internal
+        _find_node() method that searches for the node
+        that should be our parent (or finds the leaf
+        if the key is already present).
+
+        Overwrite old values if the key exists in the BTree.
+
+        Make sure to write back all changes to the disk!
+        """
+        pass
+
+    def find(self, key: KT) -> Optional[VT]:
+        """
+        Find a key and return the value associated with it.
+        If it is not in the BTree, return None.
+
+        This should be implemented with a logarithmic search
+        in the node.keys array, not a linear search. Look at the
+        BTreeNode.find_idx() method for an example of using
+        the builtin bisect library to search for a number in 
+        a sorted array in logarithmic time.
+        """
+        pass
+
+    def delete(self, key: KT) -> None:
+        raise NotImplementedError("Karma method delete()")
