316741f80ea506faa543c9d6876b7876ee8bd276
Ingrid Soto <ingrid@Ingrids-MacBook-Pro-331.local>
2022-04-18 18:53:00 -0700

    Added insertion tests

--- tests/test_btrees.py
+++ tests/test_btrees.py
@@ -4,6 +4,7 @@ from py_btrees.btree_node import BTreeNode, get_node
 
 import pytest
 from typing import Any
+import random
 
 # This is a rewriting of all of the specifications that the handout provides,
 # except it does not test the property that all leaf nodes reside at the same level.
@@ -31,8 +32,11 @@ def btree_properties_recurse(root_node_addr, node, M, L):
             assert len(node.children_addrs) >= 2
     else:
         # Non-root node properties
+        print('nonroot')
         assert node.parent_addr is not None
+        assert node.parent_addr == node.get_parent().my_addr
         assert node.index_in_parent is not None
+        assert node.get_parent().children_addrs[node.index_in_parent] == node.my_addr
         if node.is_leaf:
             assert len(node.data) >= (L+1)//2
         else:
@@ -41,6 +45,35 @@ def btree_properties_recurse(root_node_addr, node, M, L):
     # Run the assertions on all children
     for child_addr in node.children_addrs:
         btree_properties_recurse(root_node_addr, DISK.read(child_addr), M, L)
+
+#Calculate the max height of BTree
+def get_max_height(node: BTreeNode) -> int:
+    if node.is_leaf:
+        return 0
+    max_height = float('-inf')
+    for i in node.children_addrs:
+        child = get_node(i)
+        max_height = max(get_max_height(child), max_height)
+    return max_height + 1
+
+#Calculate the min height of BTree
+def get_min_height(node: BTreeNode) -> int:
+    if node.is_leaf:
+        return 0
+    min_height = float('inf')
+    for i in node.children_addrs:
+        child = get_node(i)
+        min_height = min(get_min_height(child), min_height)
+    return min_height + 1
+
+#Calculate the min height of BTree
+def get_min_height(node: BTreeNode) -> int:
+    if node.is_leaf:
+        return 0
+    min_height = float('inf')
+    for i in node.children_addrs:
+        child = get_node(i)
+        min_height = get_min_height(child)
 
 def test_btree_properties() -> None:
     M = 5
@@ -138,3 +171,143 @@ def test_insert_and_find_edge():
 
     assert btree.find(0) == "0"
     assert btree.find(1) == "1"
+
+def test_btree_properties_random_stepbystep() -> None:
+    M = random.randint(2, 10)
+    L = random.randint(1, 10)
+    btree = BTree(M, L)
+    a = [i for i in range(-1000, 1001, 1)]
+    random.shuffle(a)
+
+    for j, i in enumerate(a):
+        btree.insert(str(i), str(i))
+        assert str(i) == btree.find(str(i))
+        root_addr = btree.root_addr
+        btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
+
+    root_addr = btree.root_addr
+    btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
+
+
+def test_btree_properties_100_stepbystep() -> None:
+    M = 5
+    L = 3
+    btree = BTree(M, L)
+    for i in range(100):
+        btree.insert(i, str(i))
+        assert str(i) == btree.find(i)
+        root_addr = btree.root_addr
+        btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
+    for i in range(-1, -100, -1):
+        btree.insert(i, str(i))
+        assert str(i) == btree.find(i)
+        root_addr = btree.root_addr
+        btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
+
+    root_addr = btree.root_addr
+    btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
+
+def test_btree_properties_random_stepbystep() -> None:
+    M = random.randint(2, 10)
+    L = random.randint(1, 10)
+    btree = BTree(M, L)
+    a = [i for i in range(-1000, 1001, 1)]
+    random.shuffle(a)
+
+    for j, i in enumerate(a):
+        btree.insert(str(i), str(i))
+        assert str(i) == btree.find(str(i))
+        root_addr = btree.root_addr
+        btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
+
+    root_addr = btree.root_addr
+    btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
+
+def test_random_10_stepbystep():
+    for _ in range(10):
+	    test_btree_properties_random_stepbystep()
+
+def test_root_2_children() -> None:
+    M = 5
+    L = 3
+    btree = BTree(M, L)
+    for i in range(100):
+        btree.insert(i, str(i))
+    for i in range(0, -100):
+        btree.insert(i, str(i))
+
+    root_addr = btree.root_addr
+    root_node = get_node(root_addr)
+
+    assert len(root_node.children_addrs) >=2
+
+def test_same_level_leaves() -> None:
+    M = 5
+    L = 3
+    btree = BTree(M, L)
+    for i in range(100):
+        btree.insert(i, str(i))
+    for i in range(0, -100):
+        btree.insert(i, str(i))
+    root_addr = btree.root_addr
+    root_node = get_node(root_addr)
+    max = get_max_height(root_node)
+    min = get_min_height(root_node)
+    assert max == min
+
+#Caution. It takes a lot of time
+def test_highest_L() -> None:
+    M = 2
+    L = 99999
+    btree = BTree(M, L)
+    for i in range(99999*3):
+        btree.insert(i, str(i))
+    for i in range(0, -99999*3):
+        btree.insert(i, str(i))
+
+    root_addr = btree.root_addr
+
+    btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
+
+def test_highest_M() -> None:
+    M = 99999
+    L = 1000
+    btree = BTree(M, L)
+    for i in range(99999):
+        btree.insert(i, str(i))
+    for i in range(0, -99999):
+        btree.insert(i, str(i))
+
+    root_addr = btree.root_addr
+
+    btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
+
+def get_random_M():
+    return random.randint(2, 99999)
+
+def get_random_L():
+    return random.randint(1, 99999)
+
+def generate_M_L_pairs(n:int):
+    pairs = []
+    for i in range(n):
+        pairs.append(tuple([get_random_M(), get_random_L()]))
+    return pairs
+
+def test_different_M_L_pairs():
+    number_of_pairs = 10
+    pairs = generate_M_L_pairs(number_of_pairs)
+    while pairs:
+        M, L = pairs.pop()
+        btree = BTree(M, L)
+
+        keys = [i for i in range(-1000, 1001, 1)]
+        random.shuffle(keys)
+
+        for i in keys:
+            btree.insert(i, str(i))
+
+        root_addr = btree.root_addr
+        root_node = get_node(root_addr)
+        assert get_max_height(root_node) == get_min_height(root_node)
+        btree_properties_recurse(root_addr, DISK.read(root_addr), M, L)
